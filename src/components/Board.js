455fc7e1061708920ddc6c015446679a670e26de
csg33k <balajiyaswanth.v@gmail.com>
2022-03-20 14:08:37 +0530

    .

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,20 +5,10 @@ const Board = () => {
   const board = [];
   for(let i = 7; i >= 0; i--){
     for(let j = 1; j <= 8; j++){
-      if(i%2){
-        if(j%2){
-          board.push( <div className='tileW'>[{xAxis[i]+j}]</div> );
-        }else{
-          board.push( <div className='tileB'>[{xAxis[i]+j}]</div> );
-        }
-      }
-      else{
-        if(j%2){
-          board.push( <div className='tileB'>[{xAxis[i]+j}]</div> );
-        }else{
-          board.push( <div className='tileW'>[{xAxis[i]+j}]</div> );
-        }
-      }
+      (i%2) === 0? ((j%2) === 0? board.push( <div className='tileW'>[{xAxis[i]+j}]</div> ): 
+                                  board.push( <div className='tileB'>[{xAxis[i]+j}]</div> )):
+                    ((j%2) === 0? board.push( <div className='tileB'>[{xAxis[i]+j}]</div> ): 
+                                  board.push( <div className='tileW'>[{xAxis[i]+j}]</div> ))
     }
   }
   return (
