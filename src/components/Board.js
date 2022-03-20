65f961e9a147ec48a68a4572980cc7a39d80a8cb
csg33k <balajiyaswanth.v@gmail.com>
2022-03-20 20:45:43 +0530

    pawns set

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,9 @@ const Board = () => {
   const board = [];
   for(let i = 7; i >= 0; i--){
     for(let j = 1; j <= 8; j++){
-      (i%2) === 0? ((j%2) === 0? board.push( <div className='tileW'></div> )
+      (i%2) === 0? ((j%2) === 0? board.push( <div className='tileW'> <img src="./assets/images/white-pawn.svg" alt="black-pawn" /> </div> )
                                   :board.push( <div className='tileB'></div> ))
-                    :((j%2) === 0? board.push( <div className='tileB'></div> )
+                    :((j%2) === 0? board.push( <div className='tileB'><img src="./assets/images/black-pawn.svg" alt="black-pawn" /></div> )
                                   :board.push( <div className='tileW'></div> ))
     }
   }
