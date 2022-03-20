921050f3a3ff782cfc42757401c73f5d1417569d
csg33k <balajiyaswanth.v@gmail.com>
2022-03-20 14:03:32 +0530

    board prepped

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,28 @@
+import './Board.css'
 
 const Board = () => {
+  const xAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const board = [];
+  for(let i = 7; i >= 0; i--){
+    for(let j = 1; j <= 8; j++){
+      if(i%2){
+        if(j%2){
+          board.push( <div className='tileW'>[{xAxis[i]+j}]</div> );
+        }else{
+          board.push( <div className='tileB'>[{xAxis[i]+j}]</div> );
+        }
+      }
+      else{
+        if(j%2){
+          board.push( <div className='tileB'>[{xAxis[i]+j}]</div> );
+        }else{
+          board.push( <div className='tileW'>[{xAxis[i]+j}]</div> );
+        }
+      }
+    }
+  }
   return (
-    <div className="board"></div>
+    <div className="board">{board}</div>
   )
 }
 
