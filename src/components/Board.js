30c2516af3848d1cd0c41f78de09a6fc6beeaa78
csg33k <balajiyaswanth.v@gmail.com>
2022-03-22 15:21:10 +0530

    adding state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,27 @@
 import './Board.css';
 import Tile from './Tile';
+import { useState } from 'react';
 
 const Board = () => {
   // const xAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  const board = [];
-  for(let i = 7; i >= 0; i--){
-    for(let j = 1; j <= 8; j++){
-      board.push(<Tile row={i} column={j} />);
+
+  const initialBoardState = [];
+  const boardState = [];
+  // const [chessPieceName, setChessPieceName] = useState('hey');
+  const onSetChessPiece = (piece) => {
+    // console.log(`Image: ${piece}`);
+    return piece;
+    // console.log(chessPieceName);
+  }
+  const chessPieceName = onSetChessPiece;
+  for (let i = 7; i >= 0; i--) {
+    for (let j = 1; j <= 8; j++) {
+      initialBoardState.push(<Tile row={i} column={j} onSetChessPiece={onSetChessPiece} />);
+      boardState.push({ i, j, chessPieceName });
     }
   }
+  const [board, setBoard] = useState(initialBoardState);
+  console.log(boardState);
   return (
     <div>
       <div className="board">{board}</div>
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,17 +1,21 @@
 import './Tile.css';
+import { useState } from 'react';
 
-const Tile = ({ row, column }) => {
-    let path='/assets/images', image;
-    row === 1&& (image = '/black-pawn.svg');
-    row === 6&& (image = '/white-pawn.svg');
+const Tile = ({ row, column, onSetChessPiece }) => {
+    let path = '/assets/images', image;
+    row === 1 && (image = '/black-pawn.svg');
+    row === 6 && (image = '/white-pawn.svg');
+    (row === 0 && column === 1) && (image = '/black-rook.svg');
+    (row === 6 && column === 1) && (image = '/white-rook.svg');
 
+    onSetChessPiece(image);
     path += image;
-    console.log(path);
-    return(
-    (row%2) === 0? (    (column%2) === 0?  <div className='tile tileW' style={{backgroundImage: `url(${path})`}}></div> 
-                                  :  <div className='tile tileB' style={{backgroundImage: `url(${path})`}}></div>  )
-                    :(  (column%2) === 0?  <div className='tile tileB' style={{backgroundImage: `url(${path})`}}></div> 
-                                  : <div className='tile tileW' style={{backgroundImage: `url(${path})`}}></div>  )
+    // console.log(path);
+    return (
+        (row % 2) === 0 ? ((column % 2) === 0 ? <div className='tile tileW' style={{ backgroundImage: `url(${path})` }}></div>
+            : <div className='tile tileB' style={{ backgroundImage: `url(${path})` }}></div>)
+            : ((column % 2) === 0 ? <div className='tile tileB' style={{ backgroundImage: `url(${path})` }}></div>
+                : <div className='tile tileW' style={{ backgroundImage: `url(${path})` }}></div>)
     )
 }
 
