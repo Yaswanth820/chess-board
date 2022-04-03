8c7181ec2bd41df404086bd97e351f358b7bcacb
csg33k <balajiyaswanth.v@gmail.com>
2022-04-03 16:24:42 +0530

    .

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,6 +13,8 @@ const Board = () => {
       let image = '/no-piece';
       (i === 0 && j === 0) && (image = '/black-rook.svg');
       (i === 6 && j === 0) && (image = '/white-rook.svg');
+      // (i === 1 && j === 0) && (image = '/black-pawn.svg');
+      // (i === 7 && j === 0) && (image = '/black-pawn.svg');
       path += image;
       initialBoardState.push(<Tile row={i} column={j} image={path} />);
     }
@@ -26,6 +28,8 @@ const Board = () => {
     for (let i = 0; i <= 63 && flag === false; i++) {
       let imagePath = board[i]['props']['image'];
       let attacker = imagePath.slice(15);
+      let minDist = 1000;
+      let coordinates = [100, 100];
       if(attacker[0] === 'w'){
         console.log(`Attacker ${attacker}`);
         for(let j = 0; j <= 63 && flag === false; j++){
@@ -33,10 +37,25 @@ const Board = () => {
           let target = imagePathOfTarget.slice(15);
           if(target[0] === 'b'){
             // Rook
-            for(let k = 0; k < 8 && flag === false; k++){
+            if(board[i]['props']['row'] === board[j]['props']['row'] || board[i]['props']['column'] === board[j]['props']['column']){
+              console.log(`${attacker} killed ${target}`);
+              flag = true;
               if(board[i]['props']['row'] === board[j]['props']['row']){
-                console.log(`${attacker} killed ${target}`);
-                flag = true;
+                if(minDist > Math.abs(board[i]['props']['column'] - board[j]['props']['column'])){
+                  minDist = Math.abs(board[i]['props']['column'] - board[j]['props']['column']);
+                  coordinates[0] = board[j]['props']['row'];
+                  coordinates[1] = board[j]['props']['column'];
+                }
+              }
+                // minDist = Math.min(minDist, Math.abs(board[i]['props']['column'] - board[j]['props']['column']));
+              else{
+                if(minDist > Math.abs(board[i]['props']['row'] - board[j]['props']['row'])){
+                  minDist = Math.abs(board[i]['props']['row'] - board[j]['props']['row']);
+                  coordinates[0] = board[j]['props']['row'];
+                  coordinates[1] = board[j]['props']['column'];
+                }
+              }
+            }
                 // const left = board.slice(0, j)['props']['image'];
                 // const right = board.slice(j)['props']['image'];
                 // console.log(`left ${left}`);
@@ -45,29 +64,32 @@ const Board = () => {
                 // setBoard();
                 // board[j]['props']['image'] = board[i]['props']['image'];
                 // board[i]['props']['image'] = '/assets/images/no-piece';
-              }
-            }
+              
+          }
             // Rook
-            for(let k = 0; k < 8 && flag === false; k++){
-              if(board[i]['props']['column'] === board[j]['props']['column']){
-                console.log(`${attacker} killed ${target}`);
-                flag = true;
-                // const left = board.slice(0, j);
-                // const right = board.slice(j)['props']['image'];
-                // left.forEach(element => {
-                  // console.log(element.props);
-                // });
-                // console.log(`right ${right}`);
-                // board[j]['props']['image'] = board[i]['props']['image'];
-                // board[i]['props']['image'] = '/assets/images/no-piece';
-              }
-            }
+            // for(let k = 0; k < 8 && flag === false; k++){
+            //   if(board[i]['props']['column'] === board[j]['props']['column']){
+            //     console.log(`${attacker} killed ${target}`);
+            //     flag = true;
+            //     // console.log(j);
+            //     const left = board.slice(0, j);
+            //     const right = board.slice(j);
+            //     // console.log(`right ${right}`);
+            //     console.log(left.length + right.length);
+            //     setBoard([... left, ...right]);
+            //     console.log(board);
+            //     // board[j]['props']['image'] = board[i]['props']['image'];
+            //     // board[i]['props']['image'] = '/assets/images/no-piece';
+            //   }
+            // }
           }
         }
-        // console.log(chessPiece);
+        if(minDist != 1000){
+          console.log(minDist);
+          console.log(coordinates);
+        }
       }
     }
-  }
   else{
     // Black
     for (let i = 0; i <= 63; i++) {
