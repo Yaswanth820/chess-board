a27334931e51d492871a80fca75e1905d83331fb
csg33k <balajiyaswanth.v@gmail.com>
2022-04-03 15:04:30 +0530

    .

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,39 +5,76 @@ import { useState } from 'react';
 const Board = () => {
 
   const initialBoardState = [];
-  const boardState = [];
-  const [tboard, tboardSetState] = useState([]);
-  const pull_img = (i, j, pieceimg) => {
-    if(pieceimg != null){
-      const p = { i, j, pieceimg };
-      // console.log(p);
-      tboardSetState([...tboard, p]);
-      console.log(tboard);
-    }
-  }
-
 
   // const chessPieceName = onSetChessPiece;
   for (let i = 7; i >= 0; i--) {
-    for (let j = 1; j <= 8; j++) {
-      initialBoardState.push(<Tile row={i} column={j} imgg={pull_img} />);
-      // let nn = imgg.length;
-      // boardState.push({ i, j });
+    for (let j = 0; j <= 7; j++) {
+      let path = '/assets/images';
+      let image = '/no-piece';
+      (i === 0 && j === 0) && (image = '/black-rook.svg');
+      (i === 6 && j === 0) && (image = '/white-rook.svg');
+      path += image;
+      initialBoardState.push(<Tile row={i} column={j} image={path} />);
     }
   }
 
   const [board, setBoard] = useState(initialBoardState);
 
-  if(counter%2){
+  if(1){
     // White
-    for (let i = 0; i < tboard.length; i++) {
-      
+    let flag = false;
+    for (let i = 0; i <= 63 && flag === false; i++) {
+      let imagePath = board[i]['props']['image'];
+      let attacker = imagePath.slice(15);
+      if(attacker[0] === 'w'){
+        console.log(`Attacker ${attacker}`);
+        for(let j = 0; j <= 63 && flag === false; j++){
+          let imagePathOfTarget = board[j]['props']['image'];
+          let target = imagePathOfTarget.slice(15);
+          if(target[0] === 'b'){
+            // Rook
+            for(let k = 0; k < 8 && flag === false; k++){
+              if(board[i]['props']['row'] === board[j]['props']['row']){
+                console.log(`${attacker} killed ${target}`);
+                flag = true;
+                // const left = board.slice(0, j)['props']['image'];
+                // const right = board.slice(j)['props']['image'];
+                // console.log(`left ${left}`);
+                // console.log(`right ${right}`);
+                // setBoard();
+                // setBoard();
+                // board[j]['props']['image'] = board[i]['props']['image'];
+                // board[i]['props']['image'] = '/assets/images/no-piece';
+              }
+            }
+            // Rook
+            for(let k = 0; k < 8 && flag === false; k++){
+              if(board[i]['props']['column'] === board[j]['props']['column']){
+                console.log(`${attacker} killed ${target}`);
+                flag = true;
+                // const left = board.slice(0, j);
+                // const right = board.slice(j)['props']['image'];
+                // left.forEach(element => {
+                  // console.log(element.props);
+                // });
+                // console.log(`right ${right}`);
+                // board[j]['props']['image'] = board[i]['props']['image'];
+                // board[i]['props']['image'] = '/assets/images/no-piece';
+              }
+            }
+          }
+        }
+        // console.log(chessPiece);
+      }
     }
-  }else{
-    // Black
-
   }
-  console.log(tboard);
+  else{
+    // Black
+    for (let i = 0; i <= 63; i++) {
+      // console.log(board[i]['props']['image']);
+    }
+  }
+  // console.log(board[8]);
   return (
     <div>
       <div className="board">{board}</div>
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,24 +1,24 @@
 import './Tile.css';
 import { useState } from 'react';
 
-const Tile = ({ row, column, imgg }) => {
+const Tile = ({ row, column, image }) => {
 
     // const [image, setImage] = useState('');
-    let path = '/assets/images', image;
-    row === 1 && (image = '/black-pawn.svg');
-    row === 6 && (image = '/white-pawn.svg');
-    (row === 0 && column === 1) && (image = '/black-rook.svg');
-    (row === 6 && column === 1) && (image = '/white-rook.svg');
+    // let path = '/assets/images', image;
+    // row === 1 && (image = '/black-pawn.svg');
+    // row === 6 && (image = '/white-pawn.svg');
+    // (row === 0 && column === 1) && (image = '/black-rook.svg');
+    // (row === 6 && column === 1) && (image = '/white-rook.svg');
 
     // onSetChessPiece(image);
-    imgg(row, column, image);
-    path += image;
+    // imgg(row, column, image);
+    // path += image;
     // console.log(path);
     return (
-        (row % 2) === 0 ? ((column % 2) === 0 ? <div className='tile tileW' style={{ backgroundImage: `url(${path})` }}></div>
-            : <div className='tile tileB' style={{ backgroundImage: `url(${path})` }}></div>)
-            : ((column % 2) === 0 ? <div className='tile tileB' style={{ backgroundImage: `url(${path})` }}></div>
-                : <div className='tile tileW' style={{ backgroundImage: `url(${path})` }}></div>)
+        (row % 2) === 0 ? ((column % 2) === 0 ? <div className='tile tileW' style={{ backgroundImage: `url(${image})` }}></div>
+            : <div className='tile tileB' style={{ backgroundImage: `url(${image})` }}></div>)
+            : ((column % 2) === 0 ? <div className='tile tileB' style={{ backgroundImage: `url(${image})` }}></div>
+                : <div className='tile tileW' style={{ backgroundImage: `url(${image})` }}></div>)
     )
 }
 
